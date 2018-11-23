a6d71e3918632f37c099423d4eb765b035faab0f
Lúcás Meier <cronokirby@gmail.com>
2018-11-23 15:35:14 +0100

    Part 4.1

--- java/ch/epfl/cs107/play/game/areagame/Area.java
+++ java/ch/epfl/cs107/play/game/areagame/Area.java
@@ -2,12 +2,11 @@ package ch.epfl.cs107.play.game.areagame;
 
 import ch.epfl.cs107.play.game.Playable;
 import ch.epfl.cs107.play.game.actor.Actor;
-import ch.epfl.cs107.play.game.areagame.actor.Interactable;
 import ch.epfl.cs107.play.io.FileSystem;
-import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Transform;
+import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.window.Keyboard;
 import ch.epfl.cs107.play.window.Window;
-import ch.epfl.cs107.play.game.areagame.actor.Interactable;
 
 import java.util.List;
 
@@ -16,49 +15,76 @@ import java.util.List;
  * Area is a "Part" of the AreaGame. It is characterized by its AreaBehavior and a List of Actors
  */
 public abstract class Area implements Playable {
-
-    // Context objects
-    // TODO implements me #PROJECT #TUTO
+    // The window the game plays in
+    private Window window;
+    // The filesystem we need to access resources
+    private FileSystem filesystem;
+    /* Actor parameters */
+    // A list of actors inside the area
+    private List<Actor> actors;
+    // A list of actors requested to be added
+    private List<Actor> registeredActors;
+    // A list of actors requested to be removed
+    private List<Actor> unregisteredActors;
+    /* Camera Parameters */
+    // Actor on which the camera is centered
+    private Actor viewCandidate;
+    // Actual center of the view
+    private Vector viewCenter;
 
     /** @return (float): camera scale factor, assume it is the same in x and y direction */
     public abstract float getCameraScaleFactor();
+
+    public final void setViewCandidate(Actor a) {
+        this.viewCandidate = a;
+    }
     
     /**
      * Add an actor to the actors list
      * @param a (Actor): the actor to add, not null
-     * @param forced (Boolean): if true, the method ends
+     * @param forced (Boolean): if true, add regardless of what the area wants
      */
     private void addActor(Actor a, boolean forced) {
-        // TODO implements me #PROJECT #TUTO
+        // This is useless as long as we have no logic for
+        // refusing to add an actor, but is good as a placeholder
+        boolean add = true;
+        if (add || forced) {
+            // add on LinkedList always returns true
+           actors.add(a);
+        }
     }
 
     /**
      * Remove an actor form the actor list
      * @param a (Actor): the actor to remove, not null
-     * @param forced (Boolean): if true, the method ends
+     * @param forced (Boolean): if true, remove regardless of what the area wants
      */
     private void removeActor(Actor a, boolean forced){
-        // TODO implements me #PROJECT #TUTO
+        // Like addActor, this can contain more complex veto logic
+        boolean remove = true;
+        if (remove || forced) {
+            // This will remove based on reference equality,
+            // which is the behavior we want.
+            actors.remove(a);
+        }
     }
 
     /**
      * Register an actor : will be added at next update
      * @param a (Actor): the actor to register, not null
-     * @return (boolean): true if the actor is correctly registered
+     * @return (boolean): always true, since adding can't fail
      */
     public final boolean registerActor(Actor a){
-        // TODO implements me #PROJECT #TUTO
-        return false;
+        return registeredActors.add(a);
     }
 
     /**
      * Unregister an actor : will be removed at next update
      * @param a (Actor): the actor to unregister, not null
-     * @return (boolean): true if the actor is correctly unregistered
+     * @return (boolean): always true, since removing can't fail
      */
     public final boolean unregisterActor(Actor a){
-        // TODO implements me #PROJECT #TUTO
-        return false;
+        return unregisteredActors.add(a);
     }
 
     /**
@@ -89,7 +115,9 @@ public abstract class Area implements Playable {
 
     @Override
     public boolean begin(Window window, FileSystem fileSystem) {
-        // TODO implements me #PROJECT #TUTO
+        this.window = window;
+        this.filesystem = fileSystem;
+        this.viewCenter = Vector.ZERO;
         return true;
     }
 
@@ -103,21 +131,54 @@ public abstract class Area implements Playable {
         return true;
     }
 
-    @Override
-    public void update(float deltaTime) {
-        // TODO implements me #PROJECT #TUTO
+    // Note to TAs: making this method final instead of private void makes no
+    // sense, since package private methods can't be overriden anyways.
+    // We also don't want this method ever being called outside the class.
+    /**
+     * Add the registered actors to the concrete list, and remove
+     * the unregistered ones.
+     */
+    private void purgeRegistration() {
+        // This doesn't work at all if an actor appears multiple times
+        // in one of the lists, but let's treat that as a programmer error
+        // with bizarre behavior
+        for (Actor a : registeredActors) {
+            addActor(a, false);
+        }
+        for (Actor a : unregisteredActors) {
+            removeActor(a, false);
+        }
+        // Assigning the old lists to an empty list could potentially
+        // introduce a memory link, if we hold a reference to an intermediate
+        // node, but none of the rest. Using this method avoids that
+        registeredActors.clear();
+        unregisteredActors.clear();
     }
 
+    @Override
+    public void update(float deltaTime) {
+        // actors
+        purgeRegistration();
+        // drawing
+        updateCamera();
+    }
 
     private void updateCamera () {
-        // TODO implements me #PROJECT #TUTO
+        if (viewCandidate != null) {
+            viewCenter = viewCandidate.getPosition();
+        }
+        Transform viewTransform = Transform.I
+                .scaled(getCameraScaleFactor())
+                .translated(viewCenter);
+        window.setRelativeTransform(viewTransform);
     }
 
     /**
      * Suspend method: Can be overridden, called before resume other
      */
     public void suspend(){
-        // Do nothing by default
+        // Simply update the actor list
+        purgeRegistration();
     }
 
 
