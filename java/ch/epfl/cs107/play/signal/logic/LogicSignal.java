f133f2d74a24957728508f48d228c10c566e7dbf
Lúcás Meier <cronokirby@gmail.com>
2018-11-26 10:17:10 +0100

    Part 6.2

---
+++ java/ch/epfl/cs107/play/signal/logic/LogicSignal.java
@@ -0,0 +1,13 @@
+package ch.epfl.cs107.play.signal.logic;
+
+public abstract class LogicSignal implements Logic {
+
+    public final float getIntensity() {
+        return Logic.super.getIntensity();
+    }
+
+    @Override
+    public final float getIntensity(float t) {
+        return Logic.super.getIntensity(t);
+    }
+}
