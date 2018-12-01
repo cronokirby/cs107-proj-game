2d190942809e69ff8b268f2c305e19de939d8b7c
Lúcás Meier <cronokirby@gmail.com>
2018-12-01 22:12:50 +0100

    Add bounds checking for areas

--- java/ch/epfl/cs107/play/game/areagame/AreaBehavior.java
+++ java/ch/epfl/cs107/play/game/areagame/AreaBehavior.java
@@ -187,7 +187,10 @@ public abstract class AreaBehavior {
      */
     public boolean canEnter(Interactable entity, List<DiscreteCoordinates> coordinates) {
         for (DiscreteCoordinates coordinate : coordinates) {
-            if (!getCell(coordinate).canEnter(entity)) {
+            int x = coordinate.x;
+            int y = coordinate.y;
+            boolean oob = x < 0 || x >= width || y < 0 || y >= height;
+            if (oob || !getCell(coordinate).canEnter(entity)) {
                 return false;
             }
         }
