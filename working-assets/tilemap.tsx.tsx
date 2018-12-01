741efcbf599f3a521737ea3c846b40791bef9d05
Lúcás Meier <cronokirby@gmail.com>
2018-12-01 22:11:07 +0100

    Add tiled stuff

---
+++ working-assets/tilemap.tsx.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="untitled" tilewidth="16" tileheight="16" tilecount="12" columns="6">
+ <image source="../res/images/sprites/tileset.png" width="96" height="32"/>
+</tileset>
