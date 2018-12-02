9fb6156f00c7a6619326e03d6ad99688a1e50871
Lúcás Meier <cronokirby@gmail.com>
2018-12-02 22:47:40 +0100

    Add first two physics areas

--- java/ch/epfl/cs107/play/game/octoMAN/area/Physics.java
+++ java/ch/epfl/cs107/play/game/octoMAN/area/Physics.java
@@ -1,7 +1,9 @@
 package ch.epfl.cs107.play.game.octoMAN.area;
 
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.octoMAN.actor.Boulder;
 import ch.epfl.cs107.play.game.octoMAN.actor.StandardDoor;
+import ch.epfl.cs107.play.game.octoMAN.actor.TalkingMob;
 import ch.epfl.cs107.play.io.FileSystem;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.signal.logic.Logic;
@@ -29,9 +31,9 @@ public abstract class Physics extends OctoArea {
     protected abstract List<DiscreteCoordinates> getBoulders();
 
     /**
-     * Add all the necessary doors to this room
+     * Add all the necessary actors (besides boulders) to this area
      */
-    protected abstract void addDoors();
+    protected abstract void addActors();
 
 
     @Override
@@ -45,14 +47,16 @@ public abstract class Physics extends OctoArea {
         for (DiscreteCoordinates coordinate : getBoulders()) {
             new Boulder(this, coordinate);
         }
-        addDoors();
+        addActors();
         return superOK;
     }
+
 
     @Override
     public boolean resume(Window window, FileSystem fileSystem) {
         return begin(window, fileSystem);
     }
+
 
     private static class Physics1 extends Physics {
         private Physics1() {
@@ -69,12 +73,49 @@ public abstract class Physics extends OctoArea {
         }
 
         @Override
-        protected void addDoors() {
+        protected void addActors() {
             new StandardDoor(
                     Logic.TRUE, "LevelSelect", new DiscreteCoordinates(2, 8),
                     this, new DiscreteCoordinates(4, 0)
             );
-            new StandardDoor(Logic.FALSE, "", null, this, new DiscreteCoordinates(4, 8));
+            new StandardDoor(
+                    Logic.TRUE, "Physics2", new DiscreteCoordinates(2, 1),
+                    this, new DiscreteCoordinates(4, 8)
+            );
+            String helpText =
+                    "La Physique...c'est physique!  "
+                    + "Pousse les rochers pour avancer!"
+                    + "Si tu fais une erreur, sort et recommence!";
+            new TalkingMob(
+                    helpText, this,
+                    Orientation.DOWN, new DiscreteCoordinates(3, 2)
+            );
+        }
+    }
+
+    private static class Physics2 extends Physics {
+        private Physics2() {
+            super("Physics2");
+        }
+
+        @Override
+        protected List<DiscreteCoordinates> getBoulders() {
+            List<DiscreteCoordinates> boulders = new LinkedList<>();
+            int[] xs = { 1, 2, 3, 2, 1, 3 };
+            int[] ys = { 3, 3, 3, 4, 5, 5 };
+            for (int i = 0; i < xs.length; ++i) {
+                boulders.add(new DiscreteCoordinates(xs[i], ys[i]));
+            }
+            return boulders;
+        }
+
+        @Override
+        protected void addActors() {
+            new StandardDoor(
+                    Logic.TRUE, "Physics1", new DiscreteCoordinates(4, 7),
+                    this, new DiscreteCoordinates(2, 0)
+            );
+            new StandardDoor(Logic.FALSE, "", null, this, new DiscreteCoordinates(2, 8));
         }
     }
 
@@ -84,6 +125,7 @@ public abstract class Physics extends OctoArea {
     public static List<Physics> subRooms() {
         List<Physics> subRooms = new LinkedList<>();
         subRooms.add(new Physics1());
+        subRooms.add(new Physics2());
         return subRooms;
     }
 }
