28aed111738e024f34944cbebcf5d0be0007c710
Lúcás Meier <cronokirby@gmail.com>
2018-12-10 18:40:47 +0100

    Add second computer level

--- java/ch/epfl/cs107/play/game/octoMAN/area/Computer.java
+++ java/ch/epfl/cs107/play/game/octoMAN/area/Computer.java
@@ -34,8 +34,41 @@ public abstract class Computer extends SubRoom {
             }
             Logic allButtons = new MultipleAnd(buttons);
             new StandardDoor(
-                    allButtons, "Computer9", new DiscreteCoordinates(2, 1),
+                    Logic.TRUE, "Computer2", new DiscreteCoordinates(5, 1),
                     this, new DiscreteCoordinates(5, 8)
+            );
+        }
+    }
+
+    private static class Computer2 extends Computer {
+        private Computer2() {
+            super("Computer2");
+        }
+
+        @Override
+        protected void addActors() {
+            int[][] xs = {
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 9 },
+                { 1, 2, 3, 4, 5, 9 },
+                { 1, 6, 7, 8, 9 },
+                { 1, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+            };
+            List<Logic> buttons = new LinkedList<>();
+            for (int i = 0; i < xs.length; ++i) {
+                int y = 9 - i;
+                for (int j = 0; j < xs[i].length; ++j) {
+                    buttons.add(new PressureSwitch(this, new DiscreteCoordinates(xs[i][j], y)));
+                }
+            }
+             Logic allButtons = new MultipleAnd(buttons);
+             new StandardDoor(
+                    allButtons, "Computer9", new DiscreteCoordinates(2, 1),
+                    this, new DiscreteCoordinates(5, 10)
             );
         }
     }
@@ -59,6 +92,7 @@ public abstract class Computer extends SubRoom {
     public static List<Computer> subRooms() {
         List<Computer> subRooms = new LinkedList<>();
         subRooms.add(new Computer1());
+        subRooms.add(new Computer2());
         subRooms.add(new Computer9());
         return subRooms;
     }
