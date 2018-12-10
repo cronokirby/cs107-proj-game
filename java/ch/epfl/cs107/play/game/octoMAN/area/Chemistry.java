7eb11bc8c19b19c4457af1892643f96e586c24d3
Lúcás Meier <cronokirby@gmail.com>
2018-12-10 17:55:38 +0100

    Add more chemistry rooms

--- java/ch/epfl/cs107/play/game/octoMAN/area/Chemistry.java
+++ java/ch/epfl/cs107/play/game/octoMAN/area/Chemistry.java
@@ -28,8 +28,36 @@ public abstract class Chemistry extends SubRoom {
                     this, new DiscreteCoordinates(3, 0)
             );
             new StandardDoor(
-                    Logic.TRUE, "Chemistry9", new DiscreteCoordinates(2, 1),
+                    Logic.TRUE, "Chemistry2", new DiscreteCoordinates(3, 1),
                     this, new DiscreteCoordinates(6, 7)
+            );
+        }
+    }
+
+    private static class Chemistry2 extends Chemistry {
+        private Chemistry2() {
+            super("Chemistry2");
+        }
+
+        @Override
+        protected void addActors() {
+            new StandardDoor(
+                    Logic.TRUE, "Chemistry3", new DiscreteCoordinates(2, 1),
+                    this, new DiscreteCoordinates(4, 15)
+            );
+        }
+    }
+
+    private static class Chemistry3 extends Chemistry {
+        private Chemistry3() {
+            super("Chemistry3");
+        }
+
+        @Override
+        protected void addActors() {
+            new StandardDoor(
+                Logic.TRUE, "Chemistry9", new DiscreteCoordinates(2, 1),
+                this, new DiscreteCoordinates(5, 13)
             );
         }
     }
@@ -48,6 +76,8 @@ public abstract class Chemistry extends SubRoom {
     public static List<Chemistry> subRooms() {
         List<Chemistry> subRooms = new LinkedList<>();
         subRooms.add(new Chemistry1());
+        subRooms.add(new Chemistry2());
+        subRooms.add(new Chemistry3());
         subRooms.add(new Chemistry9());
         return subRooms;
     }
